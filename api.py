b25afd27958a9ef8c6b454e37e2d87f5a015a0ce
Nima Alikhani <nimajaune@gmail.com>
2020-05-14 19:42:08 +0200

    wip

---
+++ api.py
@@ -0,0 +1,25 @@
+import sys
+import zerorpc
+
+class BackApi(object):
+    def echo(self, text):
+        """echo any text"""
+        return text
+
+def parse_port():
+    port = 4242
+    try:
+        port = int(sys.argv[1])
+    except Exception as e:
+        pass
+    return '{}'.format(port)
+
+def main():
+    addr = 'tcp://127.0.0.1:' + parse_port()
+    s = zerorpc.Server(CalcApi())
+    s.bind(addr)
+    print('start running on {}'.format(addr))
+    s.run()
+
+if __name__ == '__main__':
+    main()
