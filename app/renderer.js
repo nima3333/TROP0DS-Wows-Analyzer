493c78e50b30103150ac94cdbffaea844ec60bf4
Nima Alikhani <nimajaune@gmail.com>
2020-05-21 12:00:21 +0200

    [-] wip, transforming python back to njs

--- app/renderer.js
+++ app/renderer.js
@@ -1,6 +1,5 @@
 const {getCurrentWindow, globalShortcut, getGlobal} = require('electron').remote;
 const path = require('path')
-const axios = require('axios');
 const fs = require('fs');
 //const zerorpc = require("zerorpc")
 
@@ -35,6 +34,7 @@ window.onload = function() {
   let dict_to_transfer = {path: true_path, key: getGlobal('sharedObj').key}
   dict_to_transfer = JSON.stringify(dict_to_transfer)
   var aa = api()
+  document.body.innerHTML = aa
 /*   client.invoke("echo", dict_to_transfer, (error, res) => {
     if(error) {
     } else {
@@ -78,7 +78,6 @@ function api () {
               </div>`);
   }
   var stra = get_html(key, pathh)
-    console.dir("stra")
     console.dir(stra)
     return `<div class="jumbotron text-center" style="padding: 0">
                       <h1>Nima's Wows Analyser</h1>
@@ -119,14 +118,37 @@ function get_html(key, pathh) {
   result2.send(null);
   result2 = JSON.parse(result2.responseText)
   result2 = result2["data"]
-  console.dir(result2)
   var name_dictionary = {}
   for (const element of result2) {
     name_dictionary[element["nickname"]] = element["account_id"]
   }
-  console.dir("name_dictionary")
-  console.dir(name_dictionary)
-  return ['e', "e"]
+
+  var requestable_ships = shipids.join("%2C")
+  var result3 = new XMLHttpRequest();
+  result3.open('GET', `https://api.worldofwarships.eu/wows/encyclopedia/ships/?application_id=${key}&fields=name%2Cdefault_profile.artillery.shells%2Ctype%2Cimages.contour&ship_id=${requestable_ships}`, false);  // `false` makes the request synchronous
+  result3.send(null);
+  result3 = JSON.parse(result3.responseText)
+  var ships_dictionary = result3["data"]
+  console.dir(ships_dictionary)
+  for (const player of players) {
+    player.push(ships_dictionary[player[0]]["type"])
+  }
+  players.sort(function(a, b) {
+    if(a[3] > b[3]) return 1;
+    if(a[3] < b[3]) return -1;
+    return 0;
+  });
+
+  var a=1
+  var b=1
+
+  var left = []
+  var right = []
+
+  for (const player of players) {
+    var hidden = False
+  }
+
 }
 
 var fct_color = (wr) =>{
