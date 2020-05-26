23acba4f52b51b67414ff4f6b338297deba80ce3
Nima Alikhani <nimajaune@gmail.com>
2020-05-26 11:00:07 +0200

    build in p

--- app/main.js
+++ app/main.js
@@ -5,68 +5,6 @@ const path = require('path')
 const Menu = electron.Menu
 const { dialog } = require('electron')
 const fs = require('fs');
-const atob = require("atob");
-
-/*************************************************************
- * py process
- *************************************************************/
-
-const PY_DIST_FOLDER = 'pycalcdist'
-const PY_FOLDER = 'pycalc'
-const PY_MODULE = 'api' // without .py suffix
-
-let pyProc = null
-let pyPort = null
-
-const guessPackaged = () => {
-  const fullPath = path.join(__dirname, PY_DIST_FOLDER)
-  return require('fs').existsSync(fullPath)
-}
-
-const getScriptPath = () => {
-  if (!guessPackaged()) {
-    return path.join(__dirname, PY_FOLDER, PY_MODULE + '.py')
-  }
-  if (process.platform === 'win32') {
-    return path.join(__dirname, PY_DIST_FOLDER, PY_MODULE, PY_MODULE + '.exe')
-  }
-  return path.join(__dirname, PY_DIST_FOLDER, PY_MODULE, PY_MODULE)
-}
-
-const selectPort = () => {
-  pyPort = 4242
-  return pyPort
-}
-
-const createPyProc = () => {
-  let script = getScriptPath()
-  let port = '' + selectPort()
-
-  if (guessPackaged()) {
-    pyProc = require('child_process').execFile(script, [port])
-  } else {
-    pyProc = require('child_process').spawn('python3', [script, port])
-  }
- 
-  if (pyProc != null) {
-    //console.log(pyProc)
-    console.log('child process success on port ' + port)
-  }
-}
-
-const exitPyProc = () => {
-  pyProc.kill()
-  pyProc = null
-  pyPort = null
-}
-
-app.on('ready', createPyProc)
-app.on('will-quit', exitPyProc)
-
-
-/*************************************************************
- * window management
- *************************************************************/
 
 let mainWindow = null
 
@@ -98,7 +36,7 @@ const createWindow = () => {
     protocol: 'file:',
     slashes: true
   }))**/
-  mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
 
   mainWindow.on('closed', () => {
     mainWindow = null
